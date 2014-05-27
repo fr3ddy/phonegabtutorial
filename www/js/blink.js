f48e186166e03ffd7a559b56274aba743fc370f1
unknown <spamdomain@gmx.net>
2014-05-28 01:03:04 +0200

    inventory works

--- www/js/blink.js
+++ www/js/blink.js
@@ -1,15 +1,15 @@
 var host = "http://192.168.178.24/";
-var Steam = require('steam');
+var db = window.openDatabase("userdb", "1.0", "User Datenbank", 1000000);
 
 $(document).ready(function() {
 	$('#login').on("touch click", function() {
-		login();
+		login($('#password').val());
 	});
 });
 
-function loadPage(page) {
+function loadBlinks() {
 	$.ajax({
-		url : host + "blinkAPI/" + page,
+		url : host + "blinkAPI/blinks",
 		// the name of the callback parameter, as specified by the YQL service
 		jsonp : "callback",
 		// tell jQuery we're expecting JSONP
@@ -24,13 +24,78 @@ function loadPage(page) {
 	});
 }
 
-function login() {
-	var bot = new Steam.SteamClient();
-	bot.logOn({
-		accountName : 'xxkingarthurxx',
-		password : 'Apfel1994!'
+function loadInventory() {
+	function queryDB(tx) {
+		tx.executeSql('SELECT * FROM USER', [], querySuccess, errorCB);
+	}
+
+	function querySuccess(tx, results) {
+		var steamid = parseInt(results.rows.item(0).id);
+		console.log(steamid);
+		$.ajax({
+			url : host + "blinkAPI/inventory/" + steamid,
+			// the name of the callback parameter, as specified by the YQL service
+			jsonp : "callback",
+			// tell jQuery we're expecting JSONP
+			dataType : "jsonp",
+			// tell YQL what we want and that we want JSON
+			success : function(response) {
+				$('#content').html();
+				$.each(response, function(key, val) {
+					$('#content').append('<h2>'+val.name+'</h2>');
+					$('#content').append('<img src="'+val.img+'" class="img-thumbnail" style="width: 100%;" />');
+					$('#content').append('<div class="row"><div class="col-xs-offset-3 col-xs-3"><b>Amount:</b> '+val.amount+'</div><div class="col-xs-3"><b>Price:</b> '+val.price+' $</div></div>');
+				});
+			}
+		});
+
+	}
+
+	function errorCB(err) {
+		alert("Error processing SQL: " + err.code);
+	}
+
+
+	db.transaction(queryDB, errorCB);
+}
+
+function loadMyBlinks() {
+
+}
+
+function login(pw) {
+	$.ajax({
+		url : host + "blinkAPI/user/" + pw,
+		// the name of the callback parameter, as specified by the YQL service
+		jsonp : "callback",
+		// tell jQuery we're expecting JSONP
+		dataType : "jsonp",
+		// tell YQL what we want and that we want JSON
+		success : function(response) {
+			console.log(response);
+			function saveUser(tx) {
+				tx.executeSql('DROP TABLE IF EXISTS USER');
+				tx.executeSql('CREATE TABLE IF NOT EXISTS USER (id unique, playeravatar , playername)');
+				tx.executeSql('INSERT INTO USER (id, playeravatar , playername) VALUES (' + response.steamid + ',"' + response.playeravatar + '" , "' + response.playername + '")');
+			}
+
+
+			db.transaction(saveUser, errorCB, successCB);
+		}
 	});
-	bot.on('loggedOn', function() {
-		alert("nice");
-	});
+}
+
+//DATABASE
+
+// Transaction error callback
+//
+function errorCB(tx, err) {
+	console.error("Error processing SQL: " + err);
+}
+
+// Transaction success callback
+//
+function successCB() {
+	console.info("success!");
+	window.location = 'blinks.html';
 }
